d4be35ba58aea0786b6d9cd2d1db5584512a64cb
kdiakonidze <kartlos_diakonidze@epam.com>
2021-12-13 23:24:30 +0400

    now on jitpack repo

--- app/src/main/java/com/example/topsnackbarapplication/MainActivity.kt
+++ app/src/main/java/com/example/topsnackbarapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.topsnackbarapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.kd.topsnackbar.Notifier
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Snackbar.make()
+
+        Notifier(this).notify("asa3333333333333sa")
     }
 }
